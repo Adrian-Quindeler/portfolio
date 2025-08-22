81b4290a6108d02c511eb665aedae803160587a6
adrian-quindeler <adrianquindeler@gmail.com>
2025-08-22 14:59:20 -0300

    Sistema de autoatendimento e ajustes do Zeus

---
+++ server/dist/index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, "../../public")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"), err => {
+        if (err) {
+            res.status(404).send("Página principal não encontrada.");
+        }
+    });
+});
+app.get("/zeus", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/html/zeus.html"), err => {
+        if (err) {
+            res.status(404).send("Página do sistema Zeus não encontrada.");
+        }
+    });
+});
+app.get("/mariaBonita", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/html/mariaBonita.html"), err => {
+        if (err) {
+            res.status(404).send("Página do projeto da Maria Bonita não encontrada.");
+        }
+    });
+});
+app.get("/autoatendimento", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/html/autoatendimento.html"), err => {
+        if (err) {
+            res.status(404).send("Página do protótipo de autoatendimento não encontrada.");
+        }
+    });
+});
+app.get("/LojaGames", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/html/LojaGames.html"), err => {
+        if (err) {
+            res.status(404).send("Página da loja online não encontrada.");
+        }
+    });
+});
+app.get("/api-java", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/html/api-java.html"), err => {
+        if (err) {
+            res.status(404).send("Página das APIs em Java não encontrada.");
+        }
+    });
+});
+app.get("/portfolio-jornalista", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/html/portfolio-jornalista.html"), err => {
+        if (err) {
+            res.status(404).send("Página do portfólio da jornalista não encontrada.");
+        }
+    });
+});
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
--- server/ts/index.ts
+++ server/ts/index.ts
@@ -30,17 +30,9 @@ app.get("/zeus", (req: Request, res: Response) => {
 });
 
 app.get("/mariaBonita", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../public/html/mariaBonita.html"), err => {
+    res.sendFile(path.join(__dirname, "../../public/html/sistema_mb.html"), err => {
         if (err) {
             res.status(404).send("Página do projeto da Maria Bonita não encontrada.");
-        }
-    });
-});
-
-app.get("/autoatendimento", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../public/html/autoatendimento.html"), err => {
-        if (err) {
-            res.status(404).send("Página do protótipo de autoatendimento não encontrada.");
         }
     });
 });
@@ -49,22 +41,6 @@ app.get("/LojaGames", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../../public/html/LojaGames.html"), err => {
         if (err) {
             res.status(404).send("Página da loja online não encontrada.");
-        }
-    });
-});
-
-app.get("/api-java", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../public/html/api-java.html"), err => {
-        if (err) {
-            res.status(404).send("Página das APIs em Java não encontrada.");
-        }
-    });
-});
-
-app.get("/portfolio-jornalista", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../public/html/portfolio-jornalista.html"), err => {
-        if (err) {
-            res.status(404).send("Página do portfólio da jornalista não encontrada.");
         }
     });
 });
